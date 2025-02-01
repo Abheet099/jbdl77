dc99e54b1079ac1d0054dbccd34cb0b62d3c3c03
fenil-shah <fenil.s@iqm.com>
2025-02-02 00:16:15 +0530

    session 25

---
+++ Session25 - Major project/ewallet/user-service/src/main/java/com/example/user/UserIdentifier.java
@@ -0,0 +1,8 @@
+package com.example.user;
+
+public enum UserIdentifier {
+
+    PAN,
+    AADHAR_CARD,
+    SERVICE_ID
+}
---
+++ Session25 - Major project/ewallet/user-service/src/main/java/com/example/user/UserService.java
@@ -0,0 +1,9 @@
+package com.example.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void createUser(UserCreateRequest userCreateRequest) {
+    }
+}
